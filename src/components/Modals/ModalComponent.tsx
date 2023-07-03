795959010bacc7b6d843cb89483ff32c1c0da8c4
Nhatdev94 <nhatdev94@gmail.com>
2023-07-04 00:20:08 +0700

    edit position title modal

--- src/components/Modals/ModalComponent.tsx
+++ src/components/Modals/ModalComponent.tsx
@@ -4,10 +4,11 @@ interface PropsInterface {
     children: JSX.Element,
     handleClose: MouseEventHandler,
     title?: string | undefined,
+    isTitleCenter?: boolean
 }
 
 const ModalComponent = (props: PropsInterface) => {
-    const { children, handleClose, title } = props
+    const { children, handleClose, title, isTitleCenter = true } = props
 
     return (
         <div className='w-screen h-screen fixed top-0 left-0 z-[99]'>
@@ -15,7 +16,7 @@ const ModalComponent = (props: PropsInterface) => {
                 <div className='w-full h-full z-10 bg-black/20 absolute top-0 left-0' onClick={handleClose}></div>
                 <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 bg-white rounded-md py-2 px-4 w-2/5 min-w-[240px] min-h-[20%]'>
                     <div className={`w-full flex items-center justify-between min-h-14 mb-2 ${title ? 'mb-4' : ''}`}>
-                        <h4 className='text-sm font-semibold text-black'>{title || ''}</h4>
+                        <h4 className={`text-sm font-semibold text-black ${isTitleCenter ? 'text-center' : ''}`} >{title || ''}</h4>
                         <div className='w-fit h-fit cursor-pointer' onClick={handleClose}>{'x'}</div>
                     </div>
                     {children}
