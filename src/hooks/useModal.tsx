c893b3bae4cff27e50c1e9c6c42d8055ffc82312
Nhatdev94 <nhatdev94@gmail.com>
2023-07-04 00:24:23 +0700

    update title modal

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -4,10 +4,11 @@ import { ModalComponent } from '../components'
 interface PropsInterface {
     modalContent: JSX.Element,
     title?: string,
+    isTitleCenter?: boolean
 }
 
 const useModal = (props: PropsInterface) => {
-    const { modalContent, title } = props
+    const { modalContent, title, isTitleCenter } = props
     const [open, setOpen] = useState<boolean>(false)
 
     const handleOpen = () => {
@@ -22,7 +23,15 @@ const useModal = (props: PropsInterface) => {
         open,
         handleOpen,
         handleClose,
-        modal: () =>  open && <ModalComponent handleClose={handleClose} title={title}>{modalContent}</ModalComponent>
+        modal: () => open && (
+            <ModalComponent
+                handleClose={handleClose}
+                title={title}
+                isTitleCenter={isTitleCenter}
+            >
+                {modalContent}
+            </ModalComponent>
+        )
     }
 }
 
