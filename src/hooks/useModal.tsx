888497cee91606a5cb2c62a8109b6a74d175f7f9
Nhatdev94 <nhatdev94@gmail.com>
2023-07-04 21:43:05 +0700

    update default

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,14 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ModalComponent } from '../components'
 
 interface PropsInterface {
+    isLoading: boolean,
     modalContent: JSX.Element,
     title?: string,
-    isTitleCenter?: boolean
+    isTitleCenter?: boolean,
 }
 
 const useModal = (props: PropsInterface) => {
-    const { modalContent, title, isTitleCenter } = props
+    const { modalContent, title, isTitleCenter, isLoading } = props
     const [open, setOpen] = useState<boolean>(false)
 
     const handleOpen = () => {
@@ -19,6 +20,15 @@ const useModal = (props: PropsInterface) => {
         setOpen(false)
     }
 
+    useEffect(() => {
+        if (open) {
+            window.document.body.style.overflow = 'hidden'
+        }
+        return () => {
+            window.document.body.style.overflow = 'unset'
+        }
+    }, [open])
+
     return {
         open,
         handleOpen,
@@ -28,6 +38,7 @@ const useModal = (props: PropsInterface) => {
                 handleClose={handleClose}
                 title={title}
                 isTitleCenter={isTitleCenter}
+                isLoading={isLoading}
             >
                 {modalContent}
             </ModalComponent>
